06f7aba55dcf099068c517c0166221e9a7278b11
Priya19999 <46641222+Priya19999@users.noreply.github.com>
2020-11-13 14:53:39 +0530

    Update Stringcalculator2.java

--- Implementations/Stringcalculator2.java
+++ Implementations/Stringcalculator2.java
@@ -1,5 +1,17 @@
 package tddforassignment;
 
 public class Stringcalculator2 {
-
+  public static int add(final String numbers) {
+		String[] numbersArray = numbers.split(",");
+		if (numbersArray.length > 2) {
+			throw new RuntimeException("Up to 2 numbers separated by comma (,) are allowed");
+		} else {
+			for (String number : numbersArray) {
+				if (!number.isEmpty()) {
+					Integer.parseInt(number);
+				}
+			}
+		}
+		return 0; 
+	}
 }
