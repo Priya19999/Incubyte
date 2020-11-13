0a3b01d3087c81a7c9f64014fb0b5bc1d466f5c9
Priya19999 <46641222+Priya19999@users.noreply.github.com>
2020-11-13 14:48:09 +0530

    Update Stringcalculator5.java

--- Implementations/Stringcalculator5.java
+++ Implementations/Stringcalculator5.java
@@ -1,5 +1,14 @@
 package tddforassignment;
 
 public class Stringcalculator5 {
-
+public static int add(final String numbers) {
+		int returnValue = 0;
+		String[] numbersArray = numbers.split(",|\n");
+		for (String number : numbersArray) {
+			if (!number.trim().isEmpty()) {
+				returnValue += Integer.parseInt(number.trim());
+			}
+		}
+		return returnValue;
+	}
 }
