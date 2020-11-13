64c013964550715402d3c29595302dc01b9f82d1
Priya19999 <46641222+Priya19999@users.noreply.github.com>
2020-11-13 14:48:50 +0530

    Update Stringcalculator4.java

--- Implementations/Stringcalculator4.java
+++ Implementations/Stringcalculator4.java
@@ -1,5 +1,17 @@
 package tddforassignment;
 
 public class Stringcalculator4 {
+  public static int add(final String numbers) {
+		int returnValue = 0;
+		String[] numbersArray = numbers.split(",");
+		for (String number : numbersArray) {
+			if (!number.trim().isEmpty()) {
+				returnValue += Integer.parseInt(number);
+			}
+		}
+		return returnValue;
+	}
+
+
 
 }
